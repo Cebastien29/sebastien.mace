5aafcc37b3264ddc036ead2dc91e9273019d12e6
Sébastien Macé <Cebastien29@gmail.com>
2026-06-23 19:11:00 +0200

    english

---
+++ Shared/include.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", async () => {
+  const components = ["header", "footer"];
+
+  await Promise.all(
+    components.map(async (id) => {
+      const res = await fetch(`/shared/${id}.html`);
+      const html = await res.text();
+      document.getElementById(id).innerHTML = html;
+    })
+  );
+
+  // IMPORTANT : après injection du header
+  const subtitle = document.body.dataset.subtitle;
+
+  const subtitleEl = document.querySelector("#page-subtitle");
+  if (subtitleEl && subtitle) {
+    subtitleEl.textContent = subtitle;
+  }
+});
