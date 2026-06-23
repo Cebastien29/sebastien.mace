3e90a06a2918df2ef3834c82c983b29a815b26dd
Sébastien Macé <Cebastien29@gmail.com>
2026-06-23 20:48:18 +0200

    Update include.js

--- Shared/include.js
+++ Shared/include.js
@@ -1,18 +1,16 @@
 window.addEventListener("DOMContentLoaded", async () => {
   const components = ["header", "footer"];
-  const base = document.querySelector('base')?.href ?? 'https://cebastien29.github.io/sebastien.mace/';
+  const root = "https://cebastien29.github.io/sebastien.mace/";
 
   await Promise.all(
     components.map(async (id) => {
-      const res = await fetch(`${base}Shared/${id}.html`);
+      const res = await fetch(`${root}Shared/${id}.html`);
       const html = await res.text();
       document.getElementById(id).innerHTML = html;
     })
   );
 
-  // IMPORTANT : après injection du header
   const subtitle = document.body.dataset.subtitle;
-
   const subtitleEl = document.querySelector("#page-subtitle");
   if (subtitleEl && subtitle) {
     subtitleEl.textContent = subtitle;
